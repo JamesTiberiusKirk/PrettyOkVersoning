95cf1d88c0b50e6e0d5e598327b590e385804373
Costin Grigore <costin000@hotmail.es>
2018-10-11 11:25:55 +0100

    Difference between files work now
    The script takes two input files from the user and shows the difference between them.

--- components/fileDiff.sh
+++ components/fileDiff.sh
@@ -16,22 +16,38 @@ while getopts h-help aflag; do
     esac
 done
 
+file1=""
+file2=""
 
-file1="${1}"
-file2="${2}"
-choice="Y"
-pos="Y"
-neg="n"
+echo "Insert the name of the first file you want to compare, followed by .txt"
+read file1
+echo "Insert the name of the second file you want to compare, followed by .txt"
+read file2
+
+if [ -f "$file1" ]
+then
+	echo "Success"
+else
+	echo "First file was not found"
+fi
+
+if [ -f "$file2" ]
+then
+else
+	echo "Second file was not found"
+fi
+
+choice=Y
 
 echo "Would you like to view the conflicts? [Y/n]"
 read choice
 echo "The choice is: $choice"
 
-if [ "$choice" == "Y" ]
+if [ "$choice"==Y ]
 then
-	vimdiff -c --report-identical-files "file1" "file2"
+	vimdiff -c --report-identical-files "$file1" "$file2"
 fi
-if [ "$choice" == "n" ]
+if [ "$choice"==n ]
 then
-differenceVar= "$(vimdiff -c --report-identical-files "file1" "file2")"
+	differenceVar= "$(vimdiff -c --report-identical-files "file1" "file2")"
 fi
