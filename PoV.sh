b22bb162957b960ffea521ad9f9e926af577975a
Unknown <dumitru.v.dv@gmail.com>
2018-10-11 13:25:02 +0100

    Some works with the main file.

--- PoV.sh
+++ PoV.sh
@@ -20,11 +20,12 @@ TRACK_LOG="repoLog/repoTrack.log"
 
 
 function printHelp(){
+    #TODO: NEED TO WORK ON THIS
     HELP_MAIN= "
     Welcome to the Pretty OK Versioning system.
     Usage:
-      ./PoV.sh [option] [flags]
-    Options and their flags:
+      ./PoV.sh [option]:[options]
+    Options and their suboptions:
       repo
         -n [name] for a new repo
         -a [file/directory name] to add a file to the version tracking
@@ -32,10 +33,22 @@ function printHelp(){
     echo $HELP_MAIN
 }
 
+function checkRepo(){
+    echo $(${REPO_STORE}${REPO_SELECT})
+    if [[ ! -a $(${REPO_STORE}${REPO_SELECT}) ]]
+    then 
+        echo "Please enter an existing repository or create a new one."
+        REPO_SELECT=""
+        exit 1
+    fi
+    echo "Repository ${REPO_SELECT} selected"
+}
+
 case $1 in
-    file) sh ${COMPPATH}${FILE} $2 $3 $4 $5;;
-    f) sh ${COMPPATH}${FILE} $2 $3 $4 $5;;
-    
+    use)
+        REPO_SELECT= $3
+        checkRepo;;
+
     user) sh ${COMPPATH}${USER} $2 $3 $4 $5;;
     u) sh ${COMPPATH}${USER} $2 $3 $4 $5;;
     
@@ -45,25 +58,14 @@ case $1 in
     check) sh ${COMPPATH}${CHECK} $2 $3 $4 $5;;
     c) sh ${COMPPATH}${CHECK} $2 $3 $4 $5;;
 
-    push) sh $REPO -p;;
+    push) sh ${COMPPATH}${REPO} push;;
+
+    new) sh ${COMPPATH}${REPO} new $3 $4 $5;;
+
+    clone) sh ${COMPPATH}${REPO} clone $3;;
+
+    add) sh ${COMPPATH}${REPO} add $3;;
+
+    "") printHelp;;
 esac
 
-while getopts u:h-help aflag; do
-    case $aflag in 
-        h) printHelp;;
-        -help) printHelp;;
-        -u) 
-            REPO_SELECT=$OPTARGS
-            checkRepo;;
-    esac
-done
-
-function checkRepo(){
-    if [[ ! -d $REPO_SELECT]]
-    then 
-        echo "Please enter an existing repository or create a new one."
-        REPO_SELECT=""
-        exit 1
-    fi
-    echo "Repository ${REPO_SELECT} selected"
-}
