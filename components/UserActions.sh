7b353fd9077bd44aa04fd86a90524af1f39c2527
Costin Grigore <costin000@hotmail.es>
2018-10-11 20:05:16 +0100

    Adding comments to the scripts

--- components/UserActions.sh
+++ components/UserActions.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-#This script checks files that have been edited and the user responsible for it
+#This script shows a file management menu to the user
+#it allows to create, edit, delete, compare and show the content of files
 
+#variable used to store the repository where the files will be stored
 repo=/home/costin/repositories/cosRepo/
 
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
@@ -16,68 +18,104 @@ echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 echo "Please choose one of the previous options"
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 
+#function used to process the user's choice
 function userChoice()
 {
 
-hello="hellooo"
-echo "$repo$hello"
+#variable used to store the choice of rhe user
 option=""
+
+#getting the choice from the user, the choice being a number between 1 and 5
 read option
+
+#if statement responsible for processing the user's choice
 if [ "$option" -eq 1 ]
 then 
 	echo "Insert the name of the file you want to create, followed by .txt:"
+	#variable used to store the name of the file to be created
 	name=""
+	#getting name for the file from the user
 	read name
+	#creating the file inside the repository variable
 	touch $repo$name
+	#if statement used to check if the file was created
 	if [ -f $repo$name ]
 	then
 		echo "File created successfully."
 	fi
+
 elif [ "$option" -eq 2 ]
 then
 	echo "Insert the name of the file you want to edit, followed by .txt:"
+	#variale used to store the name of the file to be edited
 	nameEdit=""
+	#variable to store the date and the time
 	d=$(date +%Y-%m-%d-%T-)
+	#variable used to store the name of a backup file
 	b="backup-"
+	#variable used to store the name of the user being editing the file
 	userName=$(whoami)
+	#variable to store the comments on the edited file
 	comments=""
+	#getting the name of the file to bes stored
 	read nameEdit
+	#if statement used to find the file to be edited in the repository
 	if [ -f $repo$nameEdit ]
 	then
-		echo "found"
+		echo "The file has been found!"
+		#creating a copy of the file in order to create a backup file before the file to be edited
 		cp $repo$nameEdit $repo$b$d$nameEdit
+		#allowing the user to edit the file
 		nano "$repo$nameEdit"
 	fi
+	#variable to store the name of the log file, where all the changes in the repository will be stored
 	log="logFile.txt"
+	#creating a log file inside the repository
 	touch $repo$log
-	echo "Please explain the changes done to the file"
+	echo "Please make a short description of the changes done to the file"
+	#getting the comments from the user
 	read comments
+	#saving the name of the user being editing the file, the date and the time, the comments
 	echo "Accessed by: "$userName" , Date: "$d" , Comments: "$comments" , Differences: " >> $repo$log
+	#saving the differences between the backup file and the newly edited file inside the log file inside the repository
 	diff -y $repo$nameEdit $repo$b$d$nameEdit >> $repo$log
 
 elif [ "$option" -eq 3 ]
 then 
 	echo "Insert the name of the file you want to delete, followed by .txt:"
+	#variable to store the name of the file to be deleted
 	nameDel=""
+	#getting the name of the file to be deleted
 	read nameDel
+	#deleting the file inside the repository
 	rm $repo$nameDel
+	#if statement used to check if the file has been deleted, giving an error message if it has not been deleted
 	if [ -f $repo$nameDel ]
 	then
 		echo "File has not been deleted"
 	fi
+
 elif [ "$option" -eq 4 ]
 then 
 	echo "Insert the name of the file you want to view, followed by .txt:"
+	#variable to store the name of the file to be shown its content
 	nameF=""
+	#getting the name of the file to be shown its content
 	read nameF
+	#showing content of the file from the repository
 	cat $repo$nameF
+
 elif [ "$option" -eq 5 ]
 then 
+	#calling the fileDiff script that will allow the user to observe the difference between two files
 	source fileDiff.sh
+
 else
+	#if the user has chosen a number that it is not between 1 and 5, an error message will appear, asking the user to input a valid number
 	echo "Invalid option, please try again"
+	#asking the user to input a new number
 	userChoice
 fi
 }
-
+#calling the userChoice function to allow the user to manage files in the repository
 userChoice
--- components/fileDiff.sh
+++ components/fileDiff.sh
@@ -16,53 +16,71 @@ while getopts h-help aflag; do
     esac
 done
 
+#variable in which the name of the first file will be stored
 file1=""
+#variable in which the name of the second file will be stored
 file2=""
+#variable in which the difference between two files will be stored
 differenceVar=""
 
 echo "Insert the name of the first file you want to compare, followed by .txt"
+#getting the name of the first file
 read file1
 echo "Insert the name of the second file you want to compare, followed by .txt"
+#getting the name of the second file
 read file2
-
+#if statement used to check that the first file is inside the repository
 if [ -f $repo$file1 ]
 then
-	echo "Success"
+	#if the file has been found this message will be displayed
+	echo "The first file has been found successfully."
 else
+	#if the file has not been found this message will be displayed
 	echo "First file was not found"
 fi
-
+#if statement used to check that the second file is inside the repository
 if [ -f $repo$file2 ]
 then
-	echo "Success"
+	#if the file has been found this message will be displayed
+	echo "The second file has been found successfully."
 else
+	#if the file has not been found this message will be displayed
 	echo "Second file was not found"
 fi
 
-
- checkChoice()
+#function used to check
+checkChoice()
 {
-
+#variable used to store the choice of the user
 choice=""
+#variable used to store the name of the document containing the differences between files
 d="difference.txt"
 echo "Would you like to view the conflicts? [Y/n]"
+#getting the choice from the user
 read choice
 echo "The choice is: $choice"
-
+#if statement to check if the user wants to be shown the differences between the two files
 if [ "$choice" == "Y" ]; 
 then
+	#the following lines of code show the user the differences between the two files and also store the result into the difference file inside the repository
 	diff -y $repo$file1 $repo$file2
 	diff -y $repo$file1 $repo$file2 >> $repo$d
 
 elif [ "$choice" == "n" ];
- then
+then
+	#the following line of code stores the result of the differences between the two files into the difference file inside the repository
 	diff -y $repo$file1 $repo$file2 >> $repo$d
 
 else
+	#error message telling the user to choose between the two available options
 	echo "Invalid choice, please try again:"
+	#calling the checkChoice again
 	checkChoice
 fi
 }
+#asking the user to make a choice
 checkChoice
+#the following line of code stores the result of the differences between the two files into the difference variable
 diff -y $repo$file1 $repo$file2 >> differenceVar
+#exporting the variable to be used in other scripts
 export differenceVar
